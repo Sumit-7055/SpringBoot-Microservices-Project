f048bec9413032701498ff03b4c35931e893b457
Sumit-7055 <sumit5507kumar@gmail.com>
2026-08-23 20:04:18 +0530

    Added Swagger configuration for all services

--- employee-svc/src/main/java/com/developer/sumit/employee_svc/EmployeeSvcApplication.java
+++ employee-svc/src/main/java/com/developer/sumit/employee_svc/EmployeeSvcApplication.java
@@ -1,12 +1,31 @@
 package com.developer.sumit.employee_svc;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-
+@OpenAPIDefinition(
+		info= @Info(
+				title = "Employee Service Rest APIs",
+				description = "Employee Service Rest APIs Documentation",
+				version = "v1.0,",
+				contact = @Contact(
+						name = "Sumit",
+						email = "sumit5507kr@gmail.com",
+						url = "https://sumit-7055.github.io/"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://sumit-7055.github.io/"
+				)
+		)
+)
 @SpringBootApplication
 @EnableFeignClients
 public class EmployeeSvcApplication {
