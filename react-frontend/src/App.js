8d3336439d7ef66a269b4a9cc6c3480b1fb00938
Sumit-7055 <sumit5507kumar@gmail.com>
2026-08-23 20:01:30 +0530

    updated App.js

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,12 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world</h1>
-        
 import EmployeeComponent from './component/EmployeeComponent';
 
 function App() {
